130d8d05fda5409fba9545c160bb2330ad230f31
Cookie-Kumano <cookie@kumano.moe>
2021-02-21 19:38:03 +0900

    多重起動を防止

--- YomeNotif/App.xaml.cs
+++ YomeNotif/App.xaml.cs
@@ -22,6 +22,30 @@ namespace YomeNotif
         // タスクトレイに表示するアイコン
         private NotifyIconWrapper notifyIcon;
 
+        [STAThread]
+        public static void Main()
+        {
+            const string SemaphoreName = "YomeNotif";
+            bool createdNew;
+
+            // Semaphoreクラスのインスタンス生成・保持
+            using (var semaphore
+                = new System.Threading.Semaphore(1, 1, SemaphoreName, out createdNew))
+            {
+                // 既に起動していたら、死んでもらう。
+                if (!createdNew)
+                {
+                    MessageBox.Show("嫁時報アプリは既に起動しています。タスクトレイを確認してみてください。", "嫁時報アプリ",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                App app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+
+
         // System.Windows.Application.Startupイベントを発生
         protected override void OnStartup(StartupEventArgs e)
         {
